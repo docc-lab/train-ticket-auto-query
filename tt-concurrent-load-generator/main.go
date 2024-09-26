a17dc58a83f55f23f9c68e319cfcbfb56f9b0b07
Zhaoqi(Roy) Zhang <zhaoqi.zhang@tufts.edu>
2024-09-25 21:50:58 -0400

    Bring back independent scenarios

--- tt-concurrent-load-generator/main.go
+++ tt-concurrent-load-generator/main.go
@@ -24,50 +24,38 @@ func main() {
     url := "http://192.168.188.42:8080"
     log.Printf("Connecting to: %s", url)
 
-    // scenarios := []struct {
-    //     name     string
-    //     function func(*Query)
-    // }{
-    //     {"QueryAndPreserve", QueryAndPreserve},
-    //     {"QueryAndPay", QueryAndPay},
-    //     {"QueryAndCancel", QueryAndCancel},
-    //     {"QueryAndCollect", QueryAndCollect},
-    //     {"QueryAndExecute", QueryAndExecute},
-    //     {"QueryAndConsign", QueryAndConsign},
-    //     {"QueryAndRebook", QueryAndRebook},
-    // }
-
-    // for _, scenario := range scenarios {
-    //     UpdateBaseDate() // Update BaseDate to a new random date before each scenario
-    //     log.Printf("Using BaseDate %s for scenario: %s", BaseDate.Format("2006-01-02"), scenario.name)
-
-    //     q := NewQuery(url)
-    //     log.Printf("Attempting to login for scenario: %s", scenario.name)
-    //     err = q.Login("fdse_microservice", "111111")
-    //     if err != nil {
-    //         log.Printf("Login failed for scenario %s: %v", scenario.name, err)
-    //         continue
-    //     }
-    //     log.Printf("Login successful for scenario: %s", scenario.name)
-
-    //     log.Printf("Starting scenario: %s", scenario.name)
-    //     scenario.function(q)
-    //     log.Printf("Completed scenario: %s", scenario.name)
-
-    //     time.Sleep(2 * time.Second) // Add a small delay between scenarios
-    // }
-
-    q := NewQuery(url)
-    log.Println("Attempting to login...")
-    err = q.Login("fdse_microservice", "111111")
-    if err != nil {
-        log.Fatalf("Login failed: %v", err)
+    scenarios := []struct {
+        name     string
+        function func(*Query)
+    }{
+        {"QueryAndPreserve", QueryAndPreserve},
+        {"QueryAndPay", QueryAndPay},
+        {"QueryAndCancel", QueryAndCancel},
+        {"QueryAndCollect", QueryAndCollect},
+        {"QueryAndExecute", QueryAndExecute},
+        {"QueryAndConsign", QueryAndConsign},
+        {"QueryAndRebook", QueryAndRebook},
     }
 
-    log.Println("Login successful")
+    for _, scenario := range scenarios {
+        UpdateBaseDate() // Update BaseDate to a new random date before each scenario
+        log.Printf("Using BaseDate %s for scenario: %s", BaseDate.Format("2006-01-02"), scenario.name)
 
-    // Execute scenario on current user
-    QueryAndPreserve(q)
+        q := NewQuery(url)
+        log.Printf("Attempting to login for scenario: %s", scenario.name)
+        err = q.Login("fdse_microservice", "111111")
+        if err != nil {
+            log.Printf("Login failed for scenario %s: %v", scenario.name, err)
+            continue
+        }
+        log.Printf("Login successful for scenario: %s", scenario.name)
+
+        log.Printf("Starting scenario: %s", scenario.name)
+        scenario.function(q)
+        log.Printf("Completed scenario: %s", scenario.name)
+
+        time.Sleep(2 * time.Second) // Add a small delay between scenarios
+    }
 
     // Direct query executions - uncomment to use
     // _, err = q.QueryHighSpeedTicket([2]string{"Shang Hai", "Su Zhou"}, time.Now())
